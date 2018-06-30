2dc640060baaa55dc3bb24ecc6d612f506bdcf67
wangxueliang <wangxueliang@yidian-inc.com>
2018-06-30 18:28:30 +0800

    fox

--- build/deploy_to_gh_pages.sh
+++ build/deploy_to_gh_pages.sh
@@ -11,5 +11,5 @@ git push -f "https://${GITHUB_TOKEN}@github.com/vueComponent/vue-antd-issue-help
 echo "Done updating gh-pages\n"
 
 else
- echo "Skipped updating gh-pages, because build is not triggered from the master branch."
+ echo "Skipped updating gh-pages, because build is not triggered from the master branch"
 fi;
